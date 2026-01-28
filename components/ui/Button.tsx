a8449511fe2bf6a10d82cfa369436062a46788b9
0hdora0 <0h.dora0@gmail.com>
2026-01-28 11:52:23 +0900

    색상 중복 수정

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,7 +6,7 @@ export default function Button({ text = '등록하기' }: ButtonProps) {
   return (
     <button
       type="button"
-      className="w-[90px] h-[38px] bg-brown-guide text-font-white rounded-lg font-medium text-brown-accent transition-colors"
+      className="w-22.5 h-9.5 bg-brown-guide text-font-white rounded-lg font-medium transition-colors"
     >
       {text}
     </button>
